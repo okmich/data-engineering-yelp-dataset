f5c19c5d93ccaa24c318e927c375b51c5f7a4dca
cloudera <cloudera@quickstart.cloudera>
2017-04-30 13:31:44 -0700

    after class

--- yelp_data_engineering_spark_hive/hive/yelp_db_scripts.hql
+++ yelp_data_engineering_spark_hive/hive/yelp_db_scripts.hql
@@ -24,3 +24,65 @@ create external table review (
 )
 stored as parquet
 location  '/user/cloudera/output/yelp/reviews';
+
+create external table user (
+    user_id string,
+    name string,
+    review_count bigint,
+    yelping_since string,
+    useful bigint,
+    funny bigint,
+    cool bigint,
+    fans bigint,
+    elite array<bigint>,
+    average_stars double,
+    compliment_hot bigint,
+    compliment_more bigint,
+    compliment_profile bigint,
+    compliment_cute bigint,
+    compliment_list bigint,
+    compliment_note bigint,
+    compliment_plain bigint,
+    compliment_cool bigint,
+    compliment_funny bigint,
+    compliment_writer bigint,
+    compliment_photos bigint
+)
+stored as parquet
+location  '/user/cloudera/output/yelp/user';
+
+
+create external table user_friends (
+    user_id string,
+    friend_id string
+)
+stored as parquet
+location  '/user/cloudera/output/yelp/user_friends';
+
+
+create external table business (
+    business_id string,
+    categories array<string>,
+    city string,
+    full_address string,
+    latitude double,
+    longitude double,
+    name string,
+    neighborhoods array<string>,
+    open boolean,
+    review_count bigint,
+    stars double,
+    state string
+)
+stored as parquet
+location  '/user/cloudera/output/yelp/business';
+
+
+
+
+create external table categories (
+    id int,
+    description string
+)
+stored as parquet
+location  '/user/cloudera/output/yelp/business_categories';
--- yelp_data_engineering_spark_hive/spark_note.scala
+++ yelp_data_engineering_spark_hive/spark_note.scala
@@ -7,6 +7,7 @@ refinedTipDF.write.parquet("/user/cloudera/output/yelp/tips")
 //we build the hive tip table on top of this output to allow users query our data
 
 
+import org.apache.spark.sql.Row
 
 
 //integrating spark and hive using the hive context
@@ -31,4 +32,77 @@ reviewDF.write.insertInto("yelp.review")
 
 
 // normalizing the user dataset to a many-to-many self referencing model
-val userDF = sqlContext.jsonFile("/user/cloudera/rawdata/yelp/users")
+val userDF = hiveCtx.jsonFile("/user/cloudera/rawdata/yelp/users")
+
+val friends = userDF.select("user_id", "friends").rdd
+
+
+def parseRow(row: Row) : Seq[(String, String)] ={
+	val user_Id = row.getAs[String](0)
+	val fList = row.getAs[scala.collection.mutable.WrappedArray[String]](1)
+
+	fList.map(s => (user_Id, s))
+}
+
+
+val userfriendDF = friends.flatMap(parseRow(_)).toDF
+//write into database table
+userfriendDF.write.insertInto("user_friends")
+
+
+val userMainDF = userDF.select("user_id","name","review_count","yelping_since","votes.useful","votes.funny","votes.cool","fans","elite","average_stars","compliments.hot",  "compliments.more", "compliments.profile", "compliments.cute", "compliments.list", "compliments.note", "compliments.plain", "compliments.cool", "compliments.funny", "compliments.writer", "compliments.photos")
+
+ userMainDF.write.insertInto("user")
+ 
+//  select u.user_id, u.fans, g.num from user u join 
+// (select user_id, count(1) num from user_friends group by user_id) g
+// on g.user_id = u.user_id
+
+//a simulation of incremental dataset
+val trialDF = databaseUserDF.sample(false, 0.1)
+
+val allDF = databaseUserDF.unionAll(trialDF)
+
+val newDataset = allDF.
+	map((row: Row) => (row.getAs[String](1), row)).
+	reduceByKey(function)
+
+
+//businesses
+val bizDF = hiveCtx.jsonFile("/user/cloudera/rawdata/yelp/businesses")
+
+//to get the complex schema
+val schema = bizDF.schema.prettyJson
+
+
+// import java.nio.file._
+
+// Files.write(Paths.get("file:///home/cloudera/classes/schema.json"), schema.getBytes, StandardOpenOption.CREATE_NEW)
+// val path = Paths.get(new java.net.URI("file:///home/cloudera/classes/businesses_schema.json"))
+// Files.write(path, schema.getBytes, StandardOpenOption.CREATE_NEW)
+
+bizDF.select("business_id","categories","city","full_address","latitude","longitude","name","neighborhoods","open","review_count","stars","state")
+
+val catRdd = bizDF.select("categories").rdd.flatMap(row => row.getAs[scala.collection.mutable.WrappedArray[String]](0).toSeq).
+    distinct.
+    sortBy(i => i).
+    coalesce(1).
+    mapPartitions(itr => {
+        var index = 0
+        itr map (i => {
+            index = index + 1
+            (index, i)
+        })
+    })
+
+catRdd.toDF.write.insertInto("categories")
+
+
+val bizCatRDD = bizDF.select("business_id","categories").flatMap(parseRow(_))
+
+
+//creating the many-to-many business_category
+//create businesses
+
+//make decisions on business_attributes
+
